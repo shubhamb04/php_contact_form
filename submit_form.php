2616f08dd9ab0adb51393815e558c4147f776dad
Shubham Bhavsar <shubhambhavsar50@gmail.com>
2024-03-26 19:09:49 +1100

    debugged variable typo

--- submit_form.php
+++ submit_form.php
@@ -1,23 +1,28 @@
 <?php
+
+$name = $email = $message = "";
 // Check if the form is submitted
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    $name = trim($_POST["name"]);
+    $email = trim($_POST["email"]);
+    $message = trim($_POST["message"]);
+    
     // Check if all fields are filled
-    if (!empty(htmlspecialchars( $_POST['name'])) && !empty($_POST['email']) && !empty(htmlspecialchars($_POST['message']))) {
+    if (!empty(htmlspecialchars($name)) && !empty(htmlspecialchars($email)) && !empty(htmlspecialchars($message))) {
         // Validate email
-        if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Save form data to a file
-            $data = $_POST['name'] . "|" . $_POST['email'] . "|" . $_POST['message'] . "\n";
+            $data =$name . "|" . $email . "|" . $message . "\n";
             $file = fopen("contacts.txt", "a");
             fwrite($file, $data);
             fclose($file);
-            // Display success message
+            
             echo "<p>Form successfully submitted, Thanks!</p>";
         } else {
-            // Email validation failed
             echo "<p>Invalid email format. Please enter a valid email address.</p>";
         }
     } else {
-        // Required fields are missing
         echo "<p>All fields are required.</p>";
     }
 }
